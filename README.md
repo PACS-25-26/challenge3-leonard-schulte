856cbd38beffc6a0ec3b5c42ee223dbd85a70143
Leonard Schulte <leonard.schulte@mail.polimi.it>
2026-06-01 23:25:38 +0200

    final message

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/tKSbaXxd)
-# challenge3
+
+# Challenge3
 The third challenge
+
+## Build from source
+To build from source with cmake:\
+`cmake -S . -B build -DCMAKE_BUILD_TYPE=Release`\
+`cmake --build build -j`
+
+## Run Examples
+To run the solver execute:\
+`<path to main> <input-parameter-file>`
+
+e.g. for the default case from the root directory:\
+`./bin/main examples/verification_case.ini`
+
+The results are saved as `<case name>.vtk` in the current directory.
+
+## VTK Output
+The results are exported to VTK Simple Legacy Fomat.
+Dataset Format is "Structured Points" and Dataset Attribute "Scalars".
+
+## Documentation
+
+run `doxygen docs/Doxyfile` in root directory to create doxygen documentation.
--- src/main.cpp
+++ src/main.cpp
@@ -205,12 +205,12 @@ int main(int argc, char* argv[]) {
     int k = 0;  // Nodes (linear) index
     int p = 0;  // Iteration index
 
-    double error = 1;            // Error
-    double l2 = 1;               // L2-Norm
+    double error = 1;  // Error
+    double l2    = 1;  // L2-Norm
 
     std::vector<double> source(N * N, 0.0);  // f(x,y)
-    std::vector<double> u(N*N, 0.0);         // u(x,y) stored linear with n*n Nodes
-    std::vector<double> u_new(N*N, 0.0);     // Current iteration solution (temporary)
+    std::vector<double> uh(N*N, 0.0);        // u(x,y) stored linear with n*n Nodes
+    std::vector<double> uh_new(N*N, 0.0);    // Current iteration solution (temporary)
     std::vector<double> u_exact(N*N, 0.0);   // Exact solution
 
     // Boundaryy conditions are zero: u(x=0) = 0, u(x=1) = 0, etc. -> fullfilled by initialization
@@ -232,13 +232,13 @@ int main(int argc, char* argv[]) {
                 u_exact[k] = exact_solution(x, y);
 
                 //u_new[k] = 1/(4*h*h) * (u[k-n] + u[k+n] + u[k-1] + u[k+1] + source[k]);
-                u_new[k] = 0.25 * (u[k-N] + u[k+N] + u[k-1] + u[k+1] + h*h * source[k]);
+                uh_new[k] = 0.25 * (uh[k-N] + uh[k+N] + uh[k-1] + uh[k+1] + h*h * source[k]);
             }
         }
-        error = l2_norm(u, u_new, h);
-        l2    = l2_norm(u_exact, u_new, h);
+        error = l2_norm(uh, uh_new, h);
+        l2    = l2_norm(u_exact, uh_new, h);
 
-        u = u_new;
+        uh = uh_new;
         p++;
     }
 
@@ -262,90 +262,9 @@ int main(int argc, char* argv[]) {
     std::cout << "           Elapsed Time: " << elapsed_seconds << '\n'; // C++20's chrono::duration operator<<
 
     std::cout << '\n';
-    vtk_output(N, h, u, x_min, y_min, case_name);
+    vtk_output(N, h, uh, x_min, y_min, case_name);
     vtk_output(N, h, u_exact, x_min, y_min, "analytical_solution");
     std::cout << '\n';
-
-    // Open a persistent pipe to Gnuplot
-    FILE* gnuplot1 = popen("gnuplot -persistent", "w");
-    if (!gnuplot1) {
-        std::cerr << "Error: Could not open pipe to Gnuplot." << std::endl;
-        return 1;
-    }
-
-    // Set up visual properties
-    fprintf(gnuplot1, "set title 'Numerical Solution u_h(x,y)'\n");
-    fprintf(gnuplot1, "set view map\n");           // Flat 2D heatmap projection
-    fprintf(gnuplot1, "set pm3d at b\n");          // Enable color mapping
-    fprintf(gnuplot1, "set palette rgbformulae 33,13,10\n"); 
-    fprintf(gnuplot1, "set pm3d interpolate 2,2\n"); // Smoothes the grid blocks smoothly
-
-    // Inline data stream command
-    // '-' string tells to read directly from the pipe lines
-    fprintf(gnuplot1, "splot '-' with pm3d\n");
-
-
-    // Evaluate source term and stream the coordinates
-    for (int i = 0; i < N; ++i) {
-        double x = x_min + i * h;
-        
-        for (int j = 0; j < N; ++j) {
-            k = N*i + j;
-            double y = y_min + j * h;
-            
-            fprintf(gnuplot1, "%f %f %f\n", x, y, u[k]);
-        }
-        // Segment has ended
-        fprintf(gnuplot1, "\n");
-    }
-
-    // Termination character 'e'
-    fprintf(gnuplot1, "e\n");
-
-    // Flush out buffered data strings and close the channel
-    fflush(gnuplot1);
-    pclose(gnuplot1);
-
-
-    // Open a persistent pipe to Gnuplot
-    FILE* gnuplot2 = popen("gnuplot -persistent", "w");
-    if (!gnuplot2) {
-        std::cerr << "Error: Could not open pipe to Gnuplot." << std::endl;
-        return 1;
-    }
-
-    // Set up visual properties
-    fprintf(gnuplot2, "set title 'Exact Solution u(x,y)'\n");
-    fprintf(gnuplot2, "set view map\n");           // Flat 2D heatmap projection
-    fprintf(gnuplot2, "set pm3d at b\n");          // Enable color mapping
-    fprintf(gnuplot2, "set palette rgbformulae 33,13,10\n"); 
-    fprintf(gnuplot2, "set pm3d interpolate 2,2\n"); // Smoothes the grid blocks smoothly
-
-    // Inline data stream command
-    // '-' string tells to read directly from the pipe lines
-    fprintf(gnuplot2, "splot '-' with pm3d\n");
-
-
-    // Evaluate source term and stream the coordinates
-    for (int i = 0; i < N; ++i) {
-        double x = x_min + i * h;
-        
-        for (int j = 0; j < N; ++j) {
-            k = N*i + j;
-            double y = y_min + j * h;
-            
-            fprintf(gnuplot2, "%f %f %f\n", x, y, u_exact[k]);
-        }
-        // Segment has ended
-        fprintf(gnuplot2, "\n");
-    }
-
-    // Termination character 'e'
-    fprintf(gnuplot2, "e\n");
-
-    // Flush out buffered data strings and close the channel
-    fflush(gnuplot2);
-    pclose(gnuplot2);
 
     return 0;
 }
